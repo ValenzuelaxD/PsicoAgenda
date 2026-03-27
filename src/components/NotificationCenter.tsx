b9b08b5367f47238b36d3eedb41b4f6da349fef6
Jegafloresal <jegafloresal@ittepic.edu.mx>
2026-03-27 03:10:53 -0700

    jegafloresal:"Aumento"

--- src/components/NotificationCenter.tsx
+++ src/components/NotificationCenter.tsx
@@ -229,9 +229,9 @@ export function NotificationCenter({ userType }: NotificationCenterProps) {
                         variant="ghost"
                         size="sm"
                         onClick={() => setMostrarPanel(false)}
-                        className="hover:bg-slate-700 h-10 w-10 p-0 mr-1"
+                        className={`hover:bg-slate-700 p-0 ${isMobile ? 'h-11 w-11 mr-2' : 'h-9 w-9'}`}
                       >
-                        <X className="w-5 h-5 stroke-2 text-slate-300" />
+                        <X className={`${isMobile ? 'w-6 h-6' : 'w-5 h-5'} stroke-2 text-slate-300`} />
                       </Button>
                     </div>
                   </div>
