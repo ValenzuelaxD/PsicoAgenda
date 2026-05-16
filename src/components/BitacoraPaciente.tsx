864a816b89d69d78b94e8659bf2666e79fb873b6
frjabustamantemi <frjabustamantemi@ittepic.edu.mx>
2026-05-15 21:32:06 -0700

    Francisco: Adecuar bitacora a normativa V13.

--- src/components/BitacoraPaciente.tsx
+++ src/components/BitacoraPaciente.tsx
@@ -712,6 +712,7 @@ export function BitacoraPaciente({ pacienteId }: BitacoraPacienteProps) {
                   )
                 )}
               </div>
+              </div>
             </>
           ) : (
             <Card className="bg-slate-800/50 backdrop-blur-sm border-slate-700 h-full">
